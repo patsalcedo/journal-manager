03b9f7c820132cfb00fd143c04bcd1f6acdd4659
naveendesilva <naveendesilva88@gmail.com>
2020-08-20 15:27:07 +1200

    Seperated client and server side

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080; // Step 1
+
+// HTTP request logger
+app.use(morgan('tiny'));
+
+app.get('/api', (req,res) =>{
+const data ={
+    username: 'testing',
+    age: 10
+};
+res.json(data);
+});
+
+app.get('/api/name', (req,res) =>{
+    const data ={
+        username: 'jane doe',
+        age: 38
+    };
+    res.json(data);
+    });
+
+    
+
+    
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
+    
+
+
+
