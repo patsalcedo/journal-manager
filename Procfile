ea97f7bd10aa51e4fb330befc9ea69bdaa9b4801
naveendesilva <naveendesilva88@gmail.com>
2020-08-24 19:30:38 +1200

    change .ignore file to include the build folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run heroku-postbuild
+web: npm start
---
+++ client/build/precache-manifest.ae18c808ef934a674f0da28f9afca607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9869e10e7167a0002ce927581ddd9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0c62379826c67059b3",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0c62379826c67059b3",
+    "url": "/static/js/main.b35704a4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
