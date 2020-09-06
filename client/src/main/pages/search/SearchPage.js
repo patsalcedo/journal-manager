33436015cd6815d3443df38c4ee74c510ab2cb80
nik0184 <41213901+nik0184@users.noreply.github.com>
2020-09-06 21:46:54 +1200

    Add Search Page

--- client/src/main/pages/search/SearchPage.js
+++ client/src/main/pages/search/SearchPage.js
@@ -1,15 +1,21 @@
 import React from "react";
-import "../../main/main.css";
+import "../../../main/main.css";
 
-function Search() {
+import axios from "axios";
+
+  function Search() {
   return (
     <div className="pagelayout">
       <div className="SearchPage">
         <h1>Search page</h1>
         <p>This is the search page.</p>
+        <div>
+      <input type="text" placeholder="Enter item to be searched" />
+      </div>
       </div>
     </div>
   );
 }
+
 
 export default Search;
