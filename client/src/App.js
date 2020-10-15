0823ea61ecaf057fb43568a0fa0e8d57eb4b33e4
nik0184 <41213901+nik0184@users.noreply.github.com>
2020-10-16 11:05:27 +1300

    Fix router problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./main/main.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SearchPage from "./main/pages/search/SearchPage.js"
 
 class App extends Component {
   render() {
     return (
+      <Router exact path="/searchpage" component={SearchPage}>
       <div className="app">
         <Main />
       </div>
+      </Router>
     );
   }
 }
